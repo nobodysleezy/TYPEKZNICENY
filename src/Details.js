dffddb32c3f030dc0d8773ad92c7cb39f4ebcdba
Marek Dlabaja <marecek.dlabaja@gmail.com>
2024-01-21 19:33:27 +0100

    typek varil

---
+++ src/Details.js
@@ -0,0 +1,169 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+
+export const personalDetails = {
+  name: "Zničený Týpek",
+  tagline: "Rádoby programátor",
+  img: profile,
+  about: `Lorem ipsum je označení pro standardní pseudolatinský text užívaný v grafickém designu a navrhování jako demonstrativní výplňový text při vytváření pracovních ukázek grafických návrhů`,
+};
+
+
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/",
+  github: "https://github.com/nobodysleezy",
+  twitter: "https://twitter.com/milan_pochoutka",
+  instagram: "https://www.instagram.com/marek_dlabaja",
+};
+
+
+export const workDetails = [
+  {
+    Position: "Majitel podělávaní",
+    Company: `Github`,
+    Location: "Semmering",
+    Type: "Akce na 2 hodinky",
+    Duration: "Leden 2024 - Leden 2024",
+  },
+  {
+    Position: "Na volné noze týpek",
+    Company: `Žádná`,
+    Location: "Doma",
+    Type: "Pohoda",
+    Duration: "Září 2005 - Do teď",
+  },
+  {
+    Position: "Crypto",
+    Company: `WIFI MONEY`,
+    Location: "Internet",
+    Type: "Výhledové",
+    Duration: " 2021 -  Dokud nespadne",
+  },
+];
+
+
+export const eduDetails = [
+  {
+    Position: "Student",
+    Company: "Sřední škola Baltaci s.r.o",
+    Location: "Zlín",
+    Type: "Full Time",
+    Duration: "Září 2021 - Aktuálně",
+  },
+  {
+    Position: "Žák",
+    Company: `3 zš Slovenská`,
+    Location: "Zlín",
+    Type: "Full time",
+    Duration: "Září 2012 - Červen 2021",
+  },
+];
+
+
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+
+export const projectDetails = [
+  {
+    title: "Projekt 1",
+    image: projectImage1,
+    description: `Lorem ipsum je označení pro standardní pseudolatinský text užívaný v grafickém designu a navrhování jako demonstrativní výplňový text při vytváření pracovních ukázek grafických návrhů`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Projekt 2",
+    image: projectImage2,
+    description: `Lorem ipsum je označení pro standardní pseudolatinský text užívaný v grafickém designu a navrhování jako demonstrativní výplňový text při vytváření pracovních ukázek grafických návrhů`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Projekt 3",
+    image: projectImage3,
+    description: `Lorem ipsum je označení pro standardní pseudolatinský text užívaný v grafickém designu a navrhování jako demonstrativní výplňový text při vytváření pracovních ukázek grafických návrhů`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Projekt 4",
+    image: projectImage4,
+    description: `Lorem ipsum je označení pro standardní pseudolatinský text užívaný v grafickém designu a navrhování jako demonstrativní výplňový text při vytváření pracovních ukázek grafických návrhů`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Projekt 5",
+    image: projectImage5,
+    description: `Lorem ipsum je označení pro standardní pseudolatinský text užívaný v grafickém designu a navrhování jako demonstrativní výplňový text při vytváření pracovních ukázek grafických návrhů`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Projekt  6",
+    image: projectImage6,
+    description: `Lorem ipsum je označení pro standardní pseudolatinský text užívaný v grafickém designu a navrhování jako demonstrativní výplňový text při vytváření pracovních ukázek grafických návrhů`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    githubLink: "https://github.com",
+  },
+];
+
+
+export const contactDetails = {
+  email: "znicenytypek@developer.com",
+  phone: "+420 111 222 333",
+};
